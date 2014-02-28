8b83d8562409cc856f136cd7f9e0dd11d4a76180
Mohammed Jubydul Haque <jubydul.rony@gmail.com>
2014-02-28 16:43:05 +0600

    initial commit of ebizsol

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<title>Electronic Business Solution</title>
+</head>
+<body>
+	<img src="images/banner.png" width="172" />
+    <hr />
+	<h1>Welcome to Electronic Business Solution</h1>
+    
+    <br/><br/>
+    <hr/>
+    copyright &copy; All rights reserved by eBizSol.
+</body>
+</html>
