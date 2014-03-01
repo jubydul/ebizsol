c38f09398f8d9ca24f5af507da3874911da307bf
Mohammed Jubydul Haque <jubydul.rony@gmail.com>
2014-03-01 09:35:29 +0600

    bug fix in v1.0 and ready for released as v1.1

--- index.php
+++ index.php
@@ -11,6 +11,6 @@
     
     <br/><br/>
     <hr/>
-    copyright &copy; All rights reserved by eBizSol.
+    copyright &copy; All rights reserved by eBizSol. powered by <a href="#">aTechSol.com</a>
 </body>
 </html>
